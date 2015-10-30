d730ef6fe8f850776267087197eb9d7fff21b358
gilad <giladl74@gmail.com>
2015-10-30 16:35:36 +0200

    * Added effective height calculations:   - Buoyant Plume Rise   - Momentum Plume Rise   - Fuel Fire Plume Rise * Fixed wind speed at height calculation * Added regions * Wind speed is now double

--- HotSpotHealthCode/app/src/main/java/hotspothealthcode/BL/GaussianModel.java
+++ HotSpotHealthCode/app/src/main/java/hotspothealthcode/BL/GaussianModel.java
@@ -5,35 +5,51 @@ package hotspothealthcode.BL;
  */
 public class GaussianModel {
 
+    //region Static constants
+
+    private static final double G = 9.8; // gravitational acceleration (9.8 m/s2)
+
+    //endregion
+
+    //region Static Members
+
+    //endregion
+
+    //region Members
+
     private double C = 0; // C = Time-integrated atmospheric concentration (Ci-s)/(m3).
+    private TerrainType terrainType;
     private double Q = 0; // Q = Source term (Ci).
     private double H = 0; // H = Effective release height (m).
+    private double plumePhysicalHeight; // plumePhysicalHeight - h (m)
     private double lambda = 0; // lambda = = Radioactive decay constant (s –1).
     private double x = 0; // x = Downwind distance (m).
     private double y = 0; // y = Crosswind distance (m).
     private double z = 0; // z = Vertical axis distance (m).
     private double sigmaY = 0; // SigmaY = Standard deviation of the integrated concentration distribution in the crosswind direction (m).
     private double sigmaZ = 0; // SigmaZ = Standard deviation of the integrated concentration distribution in the vertical direction (m).
-    //private double u = 0; // u = Average wind speed at the effective release height (m/s).
+    //private double u = 0; // u = Average wind speed at the effective release height (m/s). TODO: UNDERSTAND IF REQUIRED
     private double L = 0; // L = Inversion layer height (m).
     private double DFx = 0; // DF(x) = Plume Depletion factor
     private double sampleTime; // Sample time for the deviation of the integrated concentration distribution. default value 10 minutes
     private double z0; // Surface roughness height (cm) values between 3 to 300 cm.
     private double uH; // Wind speed (m/s), at height H (m)
-    private double referenceHeight; // The referance height for the wind speed (values between 2 and 100)
-    private double uRreferenceHeight;
+    private double referenceHeight; // The reference height for the wind speed (values between 2 and 100)
+    private double uReferenceHeight; // wind speed at the effective release height (m/s)
+    private PasquillStability PasquillStability; // Stability type needed for calculations
 
+    //endregion
 
-    // Stability type needed for calculations
-    private static PasquillStability PasquillStability;
+    //region C'tors
 
-    public GaussianModel(double Q,
+    public GaussianModel(TerrainType terrainType,
+                         MeteorologicalConditions meteorologicalCondition,
+                         double Q,
                          double H,
                          double lambda,
                          double x,
                          double y,
                          double z,
-                         //double u,
                          double L,
                          double DFx,
                          double z0,
@@ -41,39 +57,46 @@ public class GaussianModel {
                          double uRreferenceHeight,
                          double sampleTime)
     {
-        this.initialize(Q, H, lambda, x, y, z, L, DFx, z0, referenceHeight, uRreferenceHeight, sampleTime);
+        this.initialize(terrainType, meteorologicalCondition, Q, H, lambda, x, y, z, L, DFx, z0, referenceHeight, uRreferenceHeight, sampleTime);
     }
 
-    public GaussianModel(double Q,
+    public GaussianModel(TerrainType terrainType,
+                         MeteorologicalConditions meteorologicalCondition,
+                         double Q,
                          double H,
                          double lambda,
                          double x,
                          double y,
                          double z,
-                         //double u,
                          double L,
                          double DFx,
                          double z0,
                          double referenceHeight,
-                         double uRreferenceHeight)
+                         double uReferenceHeight)
     {
-        this.initialize(Q, H, lambda, x, y, z, L, DFx, z0, referenceHeight, uRreferenceHeight, 10);
+        this.initialize(terrainType, meteorologicalCondition, Q, H, lambda, x, y, z, L, DFx, z0, referenceHeight, uReferenceHeight, 10);
     }
 
-    private void initialize(double Q,
+    //endregion
+
+    //region Other methods
+
+    private void initialize(TerrainType terrainType,
+                            MeteorologicalConditions meteorologicalCondition,
+                            double Q,
                             double H,
                             double lambda,
                             double x,
                             double y,
                             double z,
-                            //double u,
                             double L,
                             double DFx,
                             double z0,
                             double referenceHeight,
-                            double uRreferenceHeight,
+                            double uReferenceHeight,
                             double sampleTime)
     {
+        this.terrainType = terrainType;
         this.Q = Q;
         this.H = H;
         this.lambda = lambda;
@@ -84,16 +107,34 @@ public class GaussianModel {
         this.DFx = DFx;
         this.z0 = z0;
         this.referenceHeight = referenceHeight;
-        this.uRreferenceHeight = uRreferenceHeight;
+        this.uReferenceHeight = uReferenceHeight;
         this.sampleTime = sampleTime;
+
+        this.PasquillStability = new PasquillStability(uReferenceHeight, meteorologicalCondition);
+
+        calculate();
     }
 
     public void calculate()
     {
+        double buoyantEffectiveHeight;
+        double momentumEffectiveHeight;
+
         // Sigma Z,Y calculations
-        // TODO: implement the choice logic
-        calcStandardTerrainSigmaYZ();
-        calcCityTerrainSigmaYZ();
+        calcSigmaYZ(this.terrainType);
+
+        // Calculate effective height TODO: implement the choice logic
+
+        this.H = calcFuelFireEffectiveReleaseHeight(1, 1, 1, 1); //TODO: GET REAL VALUES
+
+        buoyantEffectiveHeight = calcBouyantEffectiveReleaseHeight(this.plumePhysicalHeight, 1, 1, 1, 1, 0); // TODO: GET REAL VALUES
+        momentumEffectiveHeight = calcMomentumEffectiveReleaseHeight(this.plumePhysicalHeight, 1, 1); // TODO: GET REAL VALUES
+
+        // Take the bigger between the values
+        this.H = (buoyantEffectiveHeight > momentumEffectiveHeight ? buoyantEffectiveHeight : momentumEffectiveHeight);
+
+        // Calculate effective height wind speed
+        this.uH = calcWindSpeed(this.terrainType, this.H);
 
         // Calculate the gaussian model
         // TODO: implement the choice logic
@@ -101,6 +142,10 @@ public class GaussianModel {
         this.C = calcGasConcentrationWithInversionLayer();
     }
 
+    //endregion
+
+    /**************Calculate gas concentration********************/
+    //region Calculate gas concentration
 
     /***
      * The following Gaussian model equations determine the time-integrated
@@ -114,7 +159,7 @@ public class GaussianModel {
 
         retVal *= Math.exp(-0.5 * Math.pow(this.y / this.sigmaY, 2));
         retVal *= (Math.exp(-0.5 * Math.pow((this.z - this.H) / this.sigmaZ, 2)) +
-                   Math.exp(-0.5 * Math.pow((this.z + this.H) / this.sigmaZ, 2)));
+                Math.exp(-0.5 * Math.pow((this.z + this.H) / this.sigmaZ, 2)));
         retVal *= Math.exp((-this.lambda * this.x) / this.uH) * this.DFx;
 
         return retVal;
@@ -140,9 +185,16 @@ public class GaussianModel {
         return retVal;
     }
 
-    /**************SIGMAXY CALC********************/
+    //endregion
 
-    public void calcSigmaYZ(TerrainType terrainType)
+    /**************Deviation Calculation (sigmaXY)********************/
+    //region Deviation Calculation
+
+    /**
+     * Calculate the deviation
+     * @param terrainType - The terrain type
+     */
+    private void calcSigmaYZ(TerrainType terrainType)
     {
         switch (terrainType)
         {
@@ -167,13 +219,16 @@ public class GaussianModel {
         this.sigmaZ = this.sigmaZ * Math.pow((this.z0 / 3), 0.2);
     }
 
+    /**
+     * Calculate with standard terrain
+     */
     private void calcStandardTerrainSigmaYZ(){
 
         double parameter = 0;
         double Aparameter = 0;
         double Bparameter = 1;
 
-        switch (GaussianModel.PasquillStability.stabilityType){
+        switch (this.PasquillStability.stabilityType){
             case TYPE_A: {  parameter = 0.22; Aparameter = 0.20; Bparameter = 1; break; }
             case TYPE_B: {  parameter = 0.16; Aparameter = 0.12; Bparameter = 1; break; }
             case TYPE_C: {  parameter = 0.11; Aparameter = 0.080; Bparameter = Math.sqrt(1 + (0.0002 * this.x)); break; }
@@ -186,13 +241,16 @@ public class GaussianModel {
         this.sigmaZ = ((Aparameter * x) / Bparameter);
     }
 
+    /**
+     * Calculate with city terrain
+     */
     private void calcCityTerrainSigmaYZ(){
 
         double parameter = 0;
         double Aparameter = 0;
         double Bparameter = 1;
 
-        switch (GaussianModel.PasquillStability.stabilityType){
+        switch (this.PasquillStability.stabilityType){
             case TYPE_A: { }
             case TYPE_B: {  parameter = 0.32; Aparameter = 0.24 * Math.sqrt(1 + (0.001 * this.x)); Bparameter = 1; break; }
             case TYPE_C: {  parameter = 0.22; Aparameter = 0.20; Bparameter = 1; break; }
@@ -205,12 +263,19 @@ public class GaussianModel {
         this.sigmaZ = ((Aparameter * x) / Bparameter);
     }
 
-    /**************WIND SPEED********************/
+    //endregion
 
+    /**************Wind Speed********************/
+    //region Calculate Wind Speed
+
+    /**
+     * Calculate the standard terrain wind expo factor
+     * @return the standard terrain wind expo factor
+     */
     private double getStandardTerrainWindExpoFactor() {
         double p = 0;
 
-        switch (GaussianModel.PasquillStability.stabilityType) {
+        switch (this.PasquillStability.stabilityType) {
             case TYPE_A: {
                 p = 0.07;
                 break;
@@ -240,10 +305,14 @@ public class GaussianModel {
         return p;
     }
 
+    /**
+     * Calculate the city terrain wind expo factor
+     * @return the city terrain wind expo factor
+     */
     private double getCityTerrainWindExpoFactor() {
         double p = 0;
 
-        switch (GaussianModel.PasquillStability.stabilityType) {
+        switch (this.PasquillStability.stabilityType) {
             case TYPE_A: {
                 p = 0.15;
                 break;
@@ -273,7 +342,13 @@ public class GaussianModel {
         return p;
     }
 
-    private void calclWindSpeed(TerrainType terrainType)
+    /**
+     * Calculate the wind speed in the given height
+     * @param terrainType - The terrain type
+     * @param h - The height
+     * @return the wind speed in the given height
+     */
+    private double calcWindSpeed(TerrainType terrainType, double h)
     {
         double p = 0;
 
@@ -292,6 +367,185 @@ public class GaussianModel {
             }
         }
 
-        this.uH = this.uRreferenceHeight * Math.pow((this.H / this.referenceHeight), p);
+        return this.uReferenceHeight * Math.pow((h / this.referenceHeight), p);
     }
+
+    //endregion
+
+    /**************Effective Release Height (H)********************/
+    //region Buoyant Plume Rise
+
+    /**
+     * The method calculate the Bouyant Effective Release Height
+     * @param h - the physical height (m)
+     * @param v - stack exit velocity (m/s)
+     * @param r - stack radius (m)
+     * @param ta - ambient air temperature (deg K)
+     * @param ts - stack effluent temperature (deg K)
+     * @param flux - the flux (if 0 will use v, r, ta, ts to calculate flux)
+     * @return The Bouyant Effective Release Height
+     */
+    private double calcBouyantEffectiveReleaseHeight(double h,
+                                                     double v,
+                                                     double r,
+                                                     double ta,
+                                                     double ts,
+                                                     double flux)
+    {
+        double buoyancyFlux; // F
+        double effectiveReleaseHeight; // H
+        double x;
+        double uh; // Wind speed at physical height
+        double s;
+
+        if (flux == 0) {
+            buoyancyFlux = ((r * r) * GaussianModel.G * v) * (1 - (ta / ts));
+        }
+        else
+        {
+            buoyancyFlux = flux;
+        }
+
+        if(this.PasquillStability.stabilityType == PasquillStabilityType.TYPE_A ||
+           this.PasquillStability.stabilityType == PasquillStabilityType.TYPE_B ||
+           this.PasquillStability.stabilityType == PasquillStabilityType.TYPE_C ||
+           this.PasquillStability.stabilityType == PasquillStabilityType.TYPE_D) {
+            if (buoyancyFlux >= 55) {
+                x = 119 * Math.pow(buoyancyFlux, 0.4);
+            } else {
+                x = 49 * Math.pow(buoyancyFlux, 0.625);
+            }
+
+            if(h <= 2)
+            {
+                effectiveReleaseHeight = 0;
+
+                uh = calcWindSpeed(this.terrainType, h / 2);
+            }
+            else
+            {
+                effectiveReleaseHeight = h;
+
+                uh = calcWindSpeed(this.terrainType, h);
+            }
+
+            effectiveReleaseHeight += ((1.6 * Math.pow(buoyancyFlux, 1 / 3)) * Math.pow(x, 2 / 3)) / uh;
+        }
+        else
+        {
+            if(h <= 2)
+            {
+                effectiveReleaseHeight = 0;
+
+                uh = calcWindSpeed(this.terrainType, 2);
+            }
+            else
+            {
+                effectiveReleaseHeight = h;
+
+                uh = calcWindSpeed(this.terrainType, h);
+            }
+
+            if (this.PasquillStability.stabilityType == PasquillStabilityType.TYPE_E)
+            {
+                s = (0.020 * GaussianModel.G) / ta;
+            }
+            else // TYPE_F
+            {
+                s = (0.035 * GaussianModel.G) / ta;
+            }
+
+            if(uh > 1.4)
+            {
+                effectiveReleaseHeight += (2.6 * (Math.pow(buoyancyFlux / (uh * s), 1/3)));
+            }
+            else
+            {
+                effectiveReleaseHeight += 5 * Math.pow(buoyancyFlux, 1/4) * Math.pow(s, -3.8);
+            }
+        }
+
+        return effectiveReleaseHeight;
+    }
+
+    //endregion
+
+    //region Momentum Plume Rise
+
+    /**
+     * The method calculate the Momentum Effective Release Height
+     * @param h - the physical height (m)
+     * @param v - stack exit velocity (m/s)
+     * @param r - stack radius (m)
+     * @return The Momentum Effective Release Height
+     */
+    private double calcMomentumEffectiveReleaseHeight(double h,
+                                                      double v,
+                                                      double r)
+    {
+        double momentumFlux = 0; // F
+        double effectiveReleaseHeight = h > 2 ? h : 0; // H
+        double uh; // Wind speed at physical height
+        double s;
+
+        uh = calcWindSpeed(this.terrainType, h);
+
+        if(this.PasquillStability.stabilityType == PasquillStabilityType.TYPE_A ||
+           this.PasquillStability.stabilityType == PasquillStabilityType.TYPE_B ||
+           this.PasquillStability.stabilityType == PasquillStabilityType.TYPE_C ||
+           this.PasquillStability.stabilityType == PasquillStabilityType.TYPE_D) {
+
+            effectiveReleaseHeight += ((6 * v * r) / uh);
+        }
+        else
+        {
+            momentumFlux = 0.25 * Math.pow(2 * r * v, 2);
+
+            if (this.PasquillStability.stabilityType == PasquillStabilityType.TYPE_E)
+            {
+                s = 0.000875;
+            }
+            else // TYPE_F
+            {
+                s = 0.00175;
+            }
+
+            effectiveReleaseHeight += 1.5 * ((Math.pow(momentumFlux, 1/3) / uh) * Math.pow(s, -1/6));
+        }
+
+        return effectiveReleaseHeight;
+    }
+
+    //endregion
+
+    //region Fuel Fire Plume Rise
+
+    /**
+     * The method calculate the Fuel Fire Effective Release Height
+     * @param v - volume of fuel (gallons) burned in time, dt(s)
+     * @param t - duration of fuel fire (s)
+     * @param r - Radius of burning fuel pool (m)
+     * @param temperature - ambient air temperature (K)
+     * @return the Fuel Fire Effective Release Height
+     */
+    private double calcFuelFireEffectiveReleaseHeight(double v,
+                                                      double t,
+                                                      double r,
+                                                      double temperature)
+    {
+        double q; //heat emission rate (cal/s) (Q)
+        double flux;
+        double effectiveReleaseHeight;
+
+        q = 2.58 * Math.pow(10, 7) * (v / t);
+        flux = 0.011 * (q / temperature);
+
+        effectiveReleaseHeight = this.calcBouyantEffectiveReleaseHeight(2, 0, 0, temperature, 0, flux);
+
+        effectiveReleaseHeight = Math.pow(Math.pow(effectiveReleaseHeight, 3) + Math.pow(r / 0.6, 3), 1/3);
+        effectiveReleaseHeight -= (r / 0.6);
+
+        return effectiveReleaseHeight;
+    }
+    //endregion
 }
