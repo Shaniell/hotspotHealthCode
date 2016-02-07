5653e66adc7e80243b9ddd2c401319b432512da1
gilad <giladl74@gmail.com>
2016-02-08 00:13:54 +0200

    * added a method to concentration point class that convert the distances from kilometers to LatLng class, for the map

--- HotSpotHealthCode/app/src/main/java/hotspothealthcode/BL/AtmosphericConcentration/ConcentrationPoint.java
+++ HotSpotHealthCode/app/src/main/java/hotspothealthcode/BL/AtmosphericConcentration/ConcentrationPoint.java
@@ -1,5 +1,7 @@
 package hotspothealthcode.BL.AtmosphericConcentration;
 
+import com.google.android.gms.maps.model.LatLng;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -8,6 +10,8 @@ import org.json.JSONObject;
  */
 public class ConcentrationPoint
 {
+    private static double EARTH_RADIUS = 6371.0 * 1000;
+
     private double x;
     private double y;
     private double z;
@@ -50,5 +54,58 @@ public class ConcentrationPoint
         jsonObject.put("z", this.z);
 
         return jsonObject;
+    }
+
+    public LatLng toLatLng(LatLng startPosition, int windDirection)
+    {
+        double radWindDirection = Math.toRadians(windDirection);
+        double radStartLat = Math.toRadians(startPosition.latitude);
+        double radStartLng = Math.toRadians(startPosition.longitude);
+
+
+        // Calculate point from start point to down wind distance
+        double downWindLat = Math.asin((Math.sin(radStartLat) * Math.cos(this.x / ConcentrationPoint.EARTH_RADIUS)) +
+                                       (Math.cos(radStartLat) * Math.sin(this.x / ConcentrationPoint.EARTH_RADIUS) * Math.cos(radWindDirection)));
+
+        double downWindLng = radStartLng + Math.atan2(Math.sin(radWindDirection) *
+                                                      Math.sin(this.x / ConcentrationPoint.EARTH_RADIUS) *
+                                                      Math.cos(radStartLat),
+                                                      Math.cos(this.x / ConcentrationPoint.EARTH_RADIUS) -
+                                                      (Math.sin(radStartLat) * Math.sin(downWindLat)));
+
+        LatLng point;
+
+        if (this.y == 0)
+            point = new LatLng(Math.toDegrees(downWindLat), Math.toDegrees(downWindLng));
+        // If there is a cross wind direction, calculate new lat lng
+        else
+        {
+            double direction = windDirection;
+
+            if (this.y > 0)
+            {
+                direction += 270;
+            }
+            else
+            {
+                direction += 90;
+            }
+
+            direction = Math.toRadians(direction);
+
+            // Calculate point from down wind distance point to cross wind distance
+            double crossWindLat = Math.asin((Math.sin(downWindLat) * Math.cos(this.y / ConcentrationPoint.EARTH_RADIUS)) +
+                                            (Math.cos(downWindLat) * Math.sin(this.y / ConcentrationPoint.EARTH_RADIUS) * Math.cos(direction)));
+
+            double crossWindLng = downWindLng + Math.atan2(Math.sin(direction) *
+                                                                       Math.sin(this.y / ConcentrationPoint.EARTH_RADIUS) *
+                                                                       Math.cos(downWindLat),
+                                                                       Math.cos(this.y / ConcentrationPoint.EARTH_RADIUS) -
+                                                                       (Math.sin(downWindLat) * Math.sin(crossWindLat)));
+
+            point = new LatLng(Math.toDegrees(crossWindLat), Math.toDegrees(crossWindLng));
+        }
+
+        return point;
     }
 }
